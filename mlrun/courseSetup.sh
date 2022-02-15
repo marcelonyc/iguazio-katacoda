57bc3845acb819118ea02baa03082c1c8e690aff
Marcelo Litovsky <12512887+marcelonyc@users.noreply.github.com>
2022-02-15 06:57:20 -0500

    Updated to MLRun 0.10.0
    Now with MLRun 0.10.0

--- mlrun/courseSetup.sh
+++ mlrun/courseSetup.sh
@@ -4,14 +4,24 @@ echo "+++++++++++++++++++++++++++++++++" >>  deployment.log   2>&1
 echo "  WAITING FOR SERVICES TO START ">>  deployment.log   2>&1
 echo "+++++++++++++++++++++++++++++++++">>  deployment.log   2>&1
 
+#docker network create mlrun-network
+#docker run -it -p 8080:8080 --rm -d --network mlrun-network --name mlrun-api mlrun/mlrun-api:0.6.0
+#docker run -it -p 8081:80 --rm -d --network mlrun-network --name mlrun-ui -e MLRUN_API_PROXY_URL=http://mlrun-api:8080 mlrun/mlrun-ui:0.6.0
+#docker run -p 8070:8070 -d --rm  --network mlrun-network -v /var/run/docker.sock:/var/run/docker.sock -v /tmp:/tmp --name nuclio-dashboard quay.io/nuclio/dashboard:stable-amd64
+export SHARED_DIR=~/mlrun-data
+
+docker pull mlrun/jupyter:0.10.0
+docker pull mlrun/mlrun-ui:0.10.0
+
 docker network create mlrun-network
-docker run -it -p 8080:8080 --rm -d --network mlrun-network --name mlrun-api mlrun/mlrun-api:0.6.0
-docker run -it -p 8081:80 --rm -d --network mlrun-network --name mlrun-ui -e MLRUN_API_PROXY_URL=http://mlrun-api:8080 mlrun/mlrun-ui:0.6.0
-docker run -p 8070:8070 -d --rm  --network mlrun-network -v /var/run/docker.sock:/var/run/docker.sock -v /tmp:/tmp --name nuclio-dashboard quay.io/nuclio/dashboard:stable-amd64
+docker run -it -p 8080:8080 -p 30040:8888 --rm -d --network mlrun-network --name jupyter -v ${SHARED_DIR}:/home/jovyan/data mlrun/jupyter:0.10.0
+docker run -it -p 30050:80 --rm -d --network mlrun-network --name mlrun-ui -e MLRUN_API_PROXY_URL=http://jupyter:8080 mlrun/mlrun-ui:0.10.0
+
 update-alternatives --install /usr/local/bin/python python /usr/bin/python3.8 0
 update-alternatives --install /usr/local/bin/pip pip /usr/local/bin/pip3 0
-pip install mlrun==0.6.0
-export MLRUN_DBPATH='http://localhost:8080'
+
+pip install mlrun==0.10.0
+# export MLRUN_DBPATH='http://localhost:8080'
 echo "+++++++++++++++++++++" >>  deployment.log   2>&1
 echo "DEPLOYMENT COMPLETED"  >>  deployment.log   2>&1
 echo "+++++++++++++++++++++" >>  deployment.log   2>&1
