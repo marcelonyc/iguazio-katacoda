9fb701fad126a112e5f4f7ba7f39f1a6d31d5908
Marcelo Litovsky <12512887+marcelonyc@users.noreply.github.com>
2022-02-15 07:05:07 -0500

    Added Nuclio to deployment
    Nuclio installation added. Also set run in the background to parallelize installation

--- mlrun/courseSetup.sh
+++ mlrun/courseSetup.sh
@@ -14,8 +14,10 @@ docker pull mlrun/jupyter:0.10.0
 docker pull mlrun/mlrun-ui:0.10.0
 
 docker network create mlrun-network
-docker run -it -p 8080:8080 -p 30040:8888 --rm -d --network mlrun-network --name jupyter -v ${SHARED_DIR}:/home/jovyan/data mlrun/jupyter:0.10.0
-docker run -it -p 30050:80 --rm -d --network mlrun-network --name mlrun-ui -e MLRUN_API_PROXY_URL=http://jupyter:8080 mlrun/mlrun-ui:0.10.0
+docker run -it -p 8080:8080 -p 30040:8888 --rm -d --network mlrun-network --name jupyter -v ${SHARED_DIR}:/home/jovyan/data mlrun/jupyter:0.10.0 &
+docker run -it -p 30050:80 --rm -d --network mlrun-network --name mlrun-ui -e MLRUN_API_PROXY_URL=http://jupyter:8080 mlrun/mlrun-ui:0.10.0 &
+docker run -p 8070:8070 -d --rm  --network mlrun-network -v /var/run/docker.sock:/var/run/docker.sock -v /tmp:/tmp --name nuclio-dashboard quay.io/nuclio/dashboard:stable-amd64 &
+wait
 
 update-alternatives --install /usr/local/bin/python python /usr/bin/python3.8 0
 update-alternatives --install /usr/local/bin/pip pip /usr/local/bin/pip3 0
